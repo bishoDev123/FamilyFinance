ff3da5a7ed50df0ee88181683896b1c3173bc4ff
bishoDev123 <bisholiveenglish@gmail.com>
2025-12-03 01:24:45 +0200

    Extracted form elements into components, and changed the form to be a registration form instead of a login form, and added a few more labels. TODO: Create a login page, and setup authentication backend logic

---
+++ resources/views/components/form/button.blade.php
@@ -0,0 +1,8 @@
+@props(['type' => 'submit'])
+
+<div>
+    <button type="{{ $type }}"
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        {{ $slot }}
+    </button>
+</div>
---
+++ resources/views/components/form/form-field.blade.php
@@ -0,0 +1,8 @@
+@props(['label' => 'No label selecgted', 'name', 'type' => 'text'])
+<div>
+    <label for="{{ $name }}" class="block text-sm/6 font-medium text-gray-900">{{ $label }}</label>
+    <div class="mt-2">
+        <input id="{{ $name }}" type="{{ $type }}" name="{{ $name }}" required
+               class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
+    </div>
+</div>
