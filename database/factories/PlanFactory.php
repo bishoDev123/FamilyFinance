a7a86d744fe12d7bc229368ffc98e2e3ff052a52
bishoDev123 <bisholiveenglish@gmail.com>
2026-01-10 22:47:28 +0200

    Created the plan model, schema, factory and seeder in order to add them to the user dashboaard

---
+++ database/factories/PlanFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Plan>
+ */
+class PlanFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $month = $this->faker->monthName();
+
+        return [
+            'title' => "Plan for $month",
+            'description' => $this->faker->paragraph(),
+            'budget' => $this->faker->numberBetween(1000, 10000),
+        ];
+    }
+}
