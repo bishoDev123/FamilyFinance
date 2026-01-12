92438cf5cbf18e945775057e463ec5fa13329a7d
bishoDev123 <bisholiveenglish@gmail.com>
2026-01-13 00:01:20 +0200

    added todos in the PlanController for future reference

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -17,4 +17,32 @@ class PlanController extends Controller
         Gate::authorize('view', $plan);
         return view('plans.show', ['plan' => $plan]);
     }
+
+    public function create() {
+        // return the create plan page view
+    }
+
+    public function store(Request $request) {
+        // only authorize the user to create a plan if they have less than 6 of them
+        // validate the request parameters
+        // create a database record with the request parameters and the id of the currently logged in user
+        // redirect to the dashboard
+    }
+
+    public function edit(Plan $plan) {
+        // only authorize the user to go the edit view of a certain page if they own the plan (UX)
+    }
+
+    public function update(Request $request, Plan $plan) {
+        // authorize the user to send this post request only if they own the plan (security)
+        // validate the request
+        // update plan in question with the request parameters
+        // redirect to show page
+    }
+
+    public function destroy(Plan $plan) {
+        // authorize the user to delete the plan only if they own it
+        // delete the record of this plan from the database
+        // redirect to dashboard
+    }
 }
