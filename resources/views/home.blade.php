2352ee7d927aac61ae6f2d97218760f51b37eff7
bishoDev123 <bisholiveenglish@gmail.com>
2026-01-16 23:53:27 +0200

    hotfix for the login and register buttons appearing in home screen despite being logged in, commiting this in this branch is bad practice, will refrain from doing this in the future

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,10 +9,12 @@
         <div class="flex flex-col text-center">
             <h1 class="font-bold text-white text-4xl">Manage Your Paycheck</h1>
             <p class="text-white mt-2 mb-6">Tired of your paycheck running out without knowing what was spent, well no more!</p>
+            @guest()
             <div class="flex justify-center space-x-5">
                 <x-button href="/register">Register Now</x-button>
                 <x-button href="/login">Login</x-button>
             </div>
+            @endguest
         </div>
 
     </div>
