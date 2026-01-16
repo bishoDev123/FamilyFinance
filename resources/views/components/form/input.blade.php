3b47ea05a15ebfe34d9db2e6d9811c9feeb87979
bishoDev123 <bisholiveenglish@gmail.com>
2026-01-16 20:59:09 +0200

    made the initial design for the create plan view, will implement backend logic in the next commit

---
+++ resources/views/components/form/input.blade.php
@@ -0,0 +1,11 @@
+@props(['label' => 'Label', 'name' => 'title', 'placeholder' => 'default placeholder'])
+
+<div class="mb-4">
+    <label class="block text-gray-700 text-sm font-bold mb-2" for="{{ $name }}">
+        {{ $label }}
+    </label>
+    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="{{ $name }}" type="text" placeholder="{{ $placeholder }}">
+    <x-auth.error :field="$name" />
+</div>
+
+
--- resources/views/plans/create.blade.php
+++ resources/views/plans/create.blade.php
@@ -1,3 +1,18 @@
 <x-layout>
-    Hello from the create page
+    <div class="flex flex-col justify-center items-center mt-30">
+        <h1 class="text-4xl font-bold mb-4">Create a new budget plan</h1>
+        <div class="w-full max-w-xs">
+            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <x-form.input name="title" label="Title" placeholder="plans for august" />
+                <x-form.textarea name="description" label="Description" placeholder="Plans starting from august 23rd" />
+                <x-form.input name="Budget" label="Starting budget" placeholder="100000" />
+
+                <div class="flex items-center justify-center">
+                    <button class="bg-orange-400 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                        Create Plan
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
 </x-layout>
