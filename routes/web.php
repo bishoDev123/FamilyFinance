72014dddd6e5c1a294ac72a64821c1c4ad724602
bishoDev123 <bisholiveenglish@gmail.com>
2026-02-06 21:21:49 +0200

    cleaned up the web.php file to be more clean

--- routes/web.php
+++ routes/web.php
@@ -7,37 +7,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 
-Route::get('/dashboard', [PlanController::class, 'index'])
-    ->name('dashboard')
-    ->middleware('auth');
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [PlanController::class, 'index'])->name('dashboard');
 
-Route::get('/plans/create', [PlanController::class, 'create'])
-    ->name('plans.create')
-    ->middleware('auth');
+    Route::resource('plans', PlanController::class)
+        ->except(['index']);
 
-Route::get('/plans/{plan}/edit', [PlanController::class, 'edit'])
-    ->name('plans.edit')
-    ->middleware('auth');
-
-Route::get('/plans/{plan}', [PlanController::class, 'show'])
-    ->name('plans.show')
-    ->middleware('auth');
-
-Route::put('/plans/{plan}', [PlanController::class, 'update'])
-    ->name('plans.update')
-    ->middleware('auth');
-
-Route::delete('/plans/{plan}', [PlanController::class, 'destroy'])
-    ->name('plans.destroy')
-    ->middleware('auth');
-
-Route::post('/plans', [PlanController::class, 'store'])
-    ->name('plans.store')
-    ->middleware('auth');
-
+    Route::post('/logout', [SessionController::class, 'destroy'])->name('logout');
+});
 Route::get('/register', [RegisteredUserController::class, 'index'])->name('register');
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
 Route::get('/login', [SessionController::class, 'index'])->name('login');
 Route::post('/login', [SessionController::class, 'store']);
-Route::post('/logout', [SessionController::class, 'destroy'])->name('logout')->middleware('auth');
