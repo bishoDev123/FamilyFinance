a98cd75eec719cafe4147f58352f9483ecc480bb
bishoDev123 <bisholiveenglish@gmail.com>
2026-02-08 20:12:42 +0200

    made the first step to making roles by creating a pivot table migration for regular plan members, not only will the structure be unusual, it will also be outside my comfort zone

---
+++ database/migrations/2026_02_08_180722_create_plan_user.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Plan;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plan_user', function (Blueprint $table) {
+            $table->foreignIdFor(Plan::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->primary(['plan_id', 'user_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plan_users');
+    }
+};
