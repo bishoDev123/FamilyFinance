48fc33cfdac5688a7563ef5e645019c727f51a48
bishoDev123 <bisholiveenglish@gmail.com>
2026-02-09 18:51:16 +0200

    Changed view details button based on relation to plan, but ran into an issue
    The current logic is too ineffecient and runs poorly, it is not scalable if not eager loaded, is repetative, and teaches nothing, should change in the next commit

--- resources/views/components/plan-card.blade.php
+++ resources/views/components/plan-card.blade.php
@@ -1,16 +1,30 @@
 @props(['image' => 'https://placehold.co/600x400', 'plan'])
 
-<div class="bg-neutral-primary-soft block max-w-xs border border-default rounded-lg shadow-xs">
-    <a href="#">
-        <img class="rounded-t-base" src="{{ $image }}" alt=""/>
-    </a>
+@php
+    $url = route('plans.show', $plan->id);
+@endphp
+
+<a href="{{ $url }}"
+   class="bg-neutral-primary-soft block max-w-xs border border-default rounded-lg shadow-xs hover:shadow-md transition">
+
+    <img class="rounded-t-base" src="{{ $image }}" alt="" />
+
     <div class="p-6 text-center">
-        <a href="{{ route('plans.show', $plan->id) }}">
-            <h5 class="mt-3 mb-6 text-2xl font-semibold tracking-tight text-heading">{{ $plan->title }}</h5>
-        </a>
-        <a href="{{route('plans.show', $plan->id)}}"
-           class="inline-flex items-center shadow-sm font-medium bg-orange-400 hover:bg-orange-500 rounded-lg text-sm px-4 py-2.5">
+        <h5 class="mt-3 mb-6 text-2xl font-semibold tracking-tight text-heading">
+            {{ $plan->title }}
+        </h5>
+
+        @if(auth()->user()->ownedPlans->contains($plan))
+            <span
+                class="inline-flex items-center shadow-sm font-medium bg-orange-400 hover:bg-orange-500 rounded-lg text-sm px-4 py-2.5">
             View details
-        </a>
+        </span>
+        @elseif(auth()->user()->memberPlans->contains($plan))
+            <span
+                class="inline-flex items-center shadow-sm font-medium bg-cyan-400 hover:bg-cyan-500 rounded-lg text-sm px-4 py-2.5">
+            View details
+        @endif
+        </span>
     </div>
-</div>
+</a>
+
